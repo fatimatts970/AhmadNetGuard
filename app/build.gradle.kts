c95a23fa2e3cc1fc6e197a7deda14203b6c39d6d
fatimatts970 <anjaanshahzadi27@gmail.com>
2026-07-22 06:23:03 +0500

    Add biometric and security-crypto dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,4 +49,7 @@ dependencies {
     implementation("androidx.room:room-runtime:2.6.1")
     implementation("androidx.room:room-ktx:2.6.1")
     ksp("androidx.room:room-compiler:2.6.1")
+
+    implementation("androidx.biometric:biometric:1.1.0")
+    implementation("androidx.security:security-crypto:1.1.0-alpha06")
 }
