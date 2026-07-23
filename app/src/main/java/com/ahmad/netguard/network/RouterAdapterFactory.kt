0f6b58f7a7871a328fc6069f61b1802df8d0be6a
fatimatts970 <anjaanshahzadi27@gmail.com>
2026-07-23 15:06:20 +0500

    Enhance RouterAdapterFactory with brand support
    Refactor RouterAdapterFactory to support multiple router brands and allow custom router IP.

--- app/src/main/java/com/ahmad/netguard/network/RouterAdapterFactory.kt
+++ app/src/main/java/com/ahmad/netguard/network/RouterAdapterFactory.kt
@@ -1,11 +1,12 @@
 package com.ahmad.netguard.network
 
 object RouterAdapterFactory {
-    val instance: HuaweiRouterAdapter by lazy {
-        HuaweiRouterAdapter()
-    }
 
-    fun create(): HuaweiRouterAdapter {
-        return HuaweiRouterAdapter()
+    enum class Brand { HUAWEI }
+
+    fun create(brand: Brand = Brand.HUAWEI, routerIp: String = "192.168.100.1"): RouterAdapter {
+        return when (brand) {
+            Brand.HUAWEI -> HuaweiRouterAdapter(routerIp)
+        }
     }
 }
