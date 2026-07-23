23ada89abcbb577d629056d848679121938a849c
fatimatts970 <anjaanshahzadi27@gmail.com>
2026-07-23 12:44:47 +0500

    Refactor Device data class with new properties

--- app/src/main/java/com/ahmad/netguard/model/Device.kt
+++ app/src/main/java/com/ahmad/netguard/model/Device.kt
@@ -1,9 +1,14 @@
 package com.ahmad.netguard.model
 
 data class Device(
-    val name: String,
-    val ip: String,
-    val mac: String,
+    val macAddress: String,
+    val displayName: String,
+    val ipAddress: String,
+    val isOnline: Boolean = true,
     var isBlocked: Boolean = false,
     var isHotspotActive: Boolean = false
-)
+) {
+    val name: String get() = displayName
+    val mac: String get() = macAddress
+    val ip: String get() = ipAddress
+}
