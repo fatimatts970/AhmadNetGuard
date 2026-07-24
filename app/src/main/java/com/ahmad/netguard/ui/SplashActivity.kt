36af82f0d4ce5540b15cbfb0bfef2df3804b0186
fatimatts970 <anjaanshahzadi27@gmail.com>
2026-07-24 06:32:20 +0500

    Simplify routing in SplashActivity
    Removed credential checks and biometric prompts from SplashActivity. Now directly routes to LoginActivity.

--- app/src/main/java/com/ahmad/netguard/ui/SplashActivity.kt
+++ app/src/main/java/com/ahmad/netguard/ui/SplashActivity.kt
@@ -6,7 +6,6 @@ import android.os.Handler
 import android.os.Looper
 import androidx.appcompat.app.AppCompatActivity
 import com.ahmad.netguard.R
-import com.ahmad.netguard.network.RouterCredentialStore
 
 class SplashActivity : AppCompatActivity() {
 
@@ -20,30 +19,7 @@ class SplashActivity : AppCompatActivity() {
     }
 
     private fun routeToNextScreen() {
-        val credentialStore = RouterCredentialStore(this)
-
-        if (!credentialStore.hasSavedCredentials()) {
-            startActivity(Intent(this, LoginActivity::class.java))
-            finish()
-            return
-        }
-
-        if (BiometricHelper.canUseBiometrics(this)) {
-            BiometricHelper.prompt(
-                activity = this,
-                onSuccess = { goToDashboard() },
-                onFailure = {
-                    startActivity(Intent(this, LoginActivity::class.java))
-                    finish()
-                }
-            )
-        } else {
-            goToDashboard()
-        }
-    }
-
-    private fun goToDashboard() {
-        startActivity(Intent(this, DashboardActivity::class.java))
+        startActivity(Intent(this, LoginActivity::class.java))
         finish()
     }
 }
