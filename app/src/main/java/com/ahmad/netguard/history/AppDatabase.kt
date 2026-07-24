ea4216b225d26f95bdff720203948c7974083256
fatimatts970 <anjaanshahzadi27@gmail.com>
2026-07-24 16:37:38 +0500

    Add AppLog entity and update database version

--- app/src/main/java/com/ahmad/netguard/history/AppDatabase.kt
+++ app/src/main/java/com/ahmad/netguard/history/AppDatabase.kt
@@ -5,10 +5,11 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 
-@Database(entities = [ConnectionEvent::class, UsageRecord::class], version = 2)
+@Database(entities = [ConnectionEvent::class, UsageRecord::class, AppLog::class], version = 3)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun connectionEventDao(): ConnectionEventDao
     abstract fun usageDao(): UsageDao
+    abstract fun appLogDao(): AppLogDao
 
     companion object {
         @Volatile private var instance: AppDatabase? = null
